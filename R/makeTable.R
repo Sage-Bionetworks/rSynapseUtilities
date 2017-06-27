0cb8204cb609697f6ba08709785234b586e096ab
Benjamin Logsdon <ben.logsdon@sagebase.org>
2017-06-27 10:22:44 -0700

    improve the makeTable wrapper function

--- R/makeTable.R
+++ R/makeTable.R
@@ -1,11 +1,38 @@
-makeTable <- function(df,tableName,projectId){
-  library(synapseClient)
-  synapseLogin()
-  tcresult<-as.tableColumns(df)
+makeTable <- function(df,
+                      tableName,
+                      projectId,
+                      annos=NULL,
+                      proven=NULL){
+
+  ####df: data frame
+  ####tableName: name of the table
+  ####projectId: project where table will go
+  ####annos: optional named list of key value pairs for annoations
+  ####proven: a list with slots 'used' and 'executed' that are named list of used or executed character strings
+
+  ####returns table object
+
+  synapseClient::synapseLogin()
+  tcresult<-synapseClient::as.tableColumns(df)
   cols<-tcresult$tableColumns
   fileHandleId<-tcresult$fileHandleId
-  project<- synGet(projectId)
-  schema<-TableSchema(name=tableName, parent=project, columns=cols)
-  table<-Table(schema, fileHandleId)
-  table<-synStore(table, retrieveData=TRUE)
+  project<-synapseClient::synGet(projectId)
+  schema<-synapseClient::TableSchema(name=tableName,
+                                     parent=project,
+                                     columns=cols)
+  table<-synapseClient::Table(schema,
+                              fileHandleId)
+  if(!is.null(annos)){
+    synapseClient::synSetAnnotation(table)<-annos
+  }
+  if(!is.null(proven)){
+    table<-synapseClient::synStore(table,
+                                   retrieveData=TRUE,
+                                   used=proven$used,
+                                   executed=proven$executed)
+  }else{
+    table<-synapseClient::synStore(table,
+                                   retrieveData=TRUE)
+  }
+  return(table)
 }
